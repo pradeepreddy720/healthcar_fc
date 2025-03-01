fbb9a866cf0de840590db04ab965640935e9c9e2
Pradeep <pradee720reddy@gmail.com>
2025-03-01 14:05:50 +0530

    Empty app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { AppRoutes } from './route';
+
+function App() {
+  return (
+      // <AppRoutes />
+      <div>My app</div>
+  );
+}
+
+export default App;
---
+++ src/pages/home-page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>Testing Route</div>
+  );
+};
+
+export default  HomePage;
